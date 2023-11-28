fa3ba88f81f4e839d14906efd26d2e92f41505a4
kschuppius <kschuppius@gmail.com>
2023-11-28 11:22:26 -0500

    just an overall revert, going back
    had to revert to original, lots of work to catch up on

--- src/tv-app.js
+++ src/tv-app.js
@@ -33,34 +33,73 @@ export class TvApp extends LitElement {
         margin: 16px;
         padding: 16px;
       }
-      `
+      .listing-container {
+        justify-self: center;
+        max-width: 1344px;
+        justify-items: left;
+        display: flex;
+        flex-direction: row;
+        flex-grow: 1;
+        flex-wrap: nowrap;
+        overflow-x: auto;
+        overflow-y: auto;
+        padding-left: .5rem;
+        padding-right: .5rem;
+        text-rendering: optimizeLegibility;
+        width: 100%;
+        margin: 0 auto;
+        position: relative;
+        animation-delay: 1s;
+        animation-duration: 1s;
+        line-height: 1.5;
+        font-size: 1em;
+      }
+      h5 {
+        font-weight: 400;
+      }
+      .discord {
+        display: inline-flex;
+      }
+      .middle-page{
+        display: inline-flex;
+      }
+      .
+      `,
     ];
   }
   // LitElement rendering template of your element
   render() {
     return html`
       <h2>${this.name}</h2>
+      <div class="listing-container">
       ${
         this.listings.map(
           (item) => html`
             <tv-channel 
               title="${item.title}"
               presenter="${item.metadata.author}"
-              description = "${item.description}"
-              dialog = "${item.dialog}"
               @click="${this.itemClick}"
             >
             </tv-channel>
           `
         )
       }
-      <div>
+      </div>
+
+      <div class="middle-page">
         <!-- video -->
-        <!-- discord / chat - optional -->
+      <figure id="player-figure" class="image is-16by9">
+                <iframe id="player" class="has-ratio box p-0" width="560" height="315" src="https://www.youtube.com/embed/QJMBhXjtaYU?enablejsapi=1" title="Teaching for Now and Planning for Later - Reclaim Open Online" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen=""></iframe>
+              </figure>
+ <!-- discord / chat - optional -->
+        <div class="discord">
+              <widgetbot server="954008116800938044" channel="1106691466274803723" width="100%" height="100%" style="display: inline-block; overflow: hidden; background-color: rgb(54, 57, 62); border-radius: 7px; vertical-align: top; width: 100%; height: 100%;"><iframe title="WidgetBot Discord chat embed" allow="clipboard-write; fullscreen" src="https://e.widgetbot.io/channels/954008116800938044/1106691466274803723?api=a45a80a7-e7cf-4a79-8414-49ca31324752" style="border: none; width: 100%; height: 100%;"></iframe></widgetbot>
+              <script src="https://cdn.jsdelivr.net/npm/@widgetbot/html-embed"></script>
+            </div>
       </div>
       <!-- dialog -->
       <sl-dialog label="Dialog" class="dialog">
-        Beyonce shows the world who runs it in her viral single " Who runs the world"
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
         <sl-button slot="footer" variant="primary" @click="${this.closeDialog}">Close</sl-button>
       </sl-dialog>
     `;
